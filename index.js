870d2763822e5c942d65f3308d3fe40c4a956465
Brendan Ashworth <brendan.ashworth@me.com>
2016-08-26 11:45:22 -0700

    allow for using a temporary file as buffer
    This is the main point of the fork, to use a temporary file instead of
    an in-memory buffer for chunking the stream.
    
    For most use cases, this isn't preferable, as it increases the amount of
    I/O being done, along with CPU, in exchange for reducing the majority of
    memory usage.
    
    However, for use cases where the stream could be handling a lot of
    memory moving through it, this could be preferable in order to reduce
    the RAM usage while streaming.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var inherits = require('inherits');
 var Transform = require('readable-stream').Transform;
 var defined = require('defined');
+var fs = require('fs');
+var temp = require('tmp');
+var assert = require('assert');
 
 module.exports = Block;
 inherits(Block, Transform);
@@ -18,34 +21,83 @@ function Block (size, opts) {
     if (opts.nopad) this._zeroPadding = false;
     else this._zeroPadding = defined(opts.zeroPadding, true);
     
-    this._buffered = [];
+    // Create a temporary file for this stream to buffer in.
+    this._buffer = temp.fileSync();
+
+    // _bufferedBytes is the length of the buffer file, while _bufferPosition
+    // is the position at which we have not yet flushed the buffer downstream.
     this._bufferedBytes = 0;
+    this._bufferPosition = 0;
 }
 
 Block.prototype._transform = function (buf, enc, next) {
     this._bufferedBytes += buf.length;
-    this._buffered.push(buf);
-    
-    while (this._bufferedBytes >= this.size) {
-        var b = Buffer.concat(this._buffered);
-        this._bufferedBytes -= this.size;
-        this.push(b.slice(0, this.size));
-        this._buffered = [ b.slice(this.size, b.length) ];
-    }
-    next();
+
+    // Write this new data to the temporary file.
+    var stream = this;
+    fs.write(this._buffer.fd, buf, 0, buf.length, this._bufferBytes, function(err) {
+        if (err) {
+            return next(err);
+        }
+
+        // Try to flush data downstream.
+        tryFlush(stream, next);
+    });    
 };
 
-Block.prototype._flush = function () {
-    if (this._bufferedBytes && this._zeroPadding) {
-        var zeroes = new Buffer(this.size - this._bufferedBytes);
-        zeroes.fill(0);
-        this._buffered.push(zeroes);
-        this.push(Buffer.concat(this._buffered));
-        this._buffered = null;
+// Try to flush from the underlying file downstream — unrelated to #_flush().
+// This function will recur in the event loop until we cannot flush anymore.
+function tryFlush(stream, next) {
+    // Stop recursion if we don't want to flush anymore.
+    if (stream._bufferedBytes < stream.size) {
+        return next();
     }
-    else if (this._bufferedBytes) {
-        this.push(Buffer.concat(this._buffered));
-        this._buffered = null;
-    }
-    this.push(null);
+
+    var buf = new Buffer(stream.size);
+    fs.read(stream._buffer.fd, buf, 0, buf.length, stream._bufferPosition, function(err, bytesRead, buffer) {
+        if (err) {
+            return next(err);
+        }
+
+        // This is a must-have. It should always be this way, unless the temporary file was
+        // modified under our noses.
+        assert(bytesRead == stream.size, 'block-stream-file: bytesRead must equal buffering size');
+
+        stream._bufferedBytes -= bytesRead;
+        stream._bufferPosition += bytesRead;
+
+        stream.push(buffer);
+
+        // We flushed one chunk, but may have more available. Try again?
+        tryFlush(stream, next);
+    });
+}
+
+Block.prototype._flush = function (callback) {
+    // Nothing to do.
+    if (!this._bufferedBytes) return callback();
+
+    // If we want to pad with zeroes, instantiate a Buffer first.
+    var zeroes = this._zeroPadding ? (new Buffer(this.size)).fill(0) : new Buffer(this._bufferedBytes);
+
+    // Read into the buffer filled with zeroes, so that we overwrite the zeroes with
+    // data that we have but leave the rest zeroed out.
+    // If we pass null, we'll only get whats left, without the zeroes.
+    var stream = this;
+    fs.read(this._buffer.fd, zeroes, 0, this._bufferedBytes, this._bufferPosition, function(err, bytesRead, buffer) {
+        if (err) {
+            return callback(err);
+        }
+
+        // Check to be sure we read the right amount.
+        assert(bytesRead == stream._bufferedBytes, 'block-stream-file: bytesRead must equal left bytes');
+
+        stream.push(buffer);
+        stream.push(null);
+
+        // Remove the temporary file.
+        stream._buffer.removeCallback();
+
+        callback();
+    });
 };
